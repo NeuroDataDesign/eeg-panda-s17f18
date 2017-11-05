299bc0c8c9da58133e6506e37036f4cdc04eb77c
Ryan Marren <rmarren1@jhu.edu>
2017-11-05 15:17:49 -0500

    building and pushing docs

--- build-docs.sh
+++ build-docs.sh
@@ -10,10 +10,10 @@ git commit -m "building and pushing docs"
 git push origin master
 # switch branches and pull the data we want
 git checkout gh-pages
-rm -rf .
+rm -rf ./*
 touch .nojekyll
 git checkout master docs/_build/html
-mv -r ./docs/_build/html/* ./
+mv ./docs/_build/html/* ./
 rm -rf ./docs
 git add -A
 git commit -m "publishing updated docs..."
