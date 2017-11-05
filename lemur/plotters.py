0026664891c60a0eace53fb6cb373e18c66f4f68
Ryan Marren <rmarren1@jhu.edu>
2017-11-04 20:00:29 -0400

    parallel coordinate plot added to plotters

--- lemur/plotters.py
+++ lemur/plotters.py
@@ -3,6 +3,8 @@ import plotly.graph_objs as go
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from ipywidgets import interact
 
 class DistanceMatrixPlotter:
 
@@ -38,10 +40,10 @@ class DistanceMatrixHeatmap(DistanceMatrixPlotter):
         iplot(fig)
 
 class Embedding2DScatter(DistanceMatrixPlotter):
-    titlestring = "%s embedding of %s distance matrix 2D scatter"
+    titlestring = "%s 2D %s Embedding Scatter under %s metric"
 
     def plot(self, embedder):
-        title = self.titlestring % (embedder.embedding_name, self.metric_name)
+        title = self.titlestring % (self.dataset_name, embedder.embedding_name, self.metric_name)
         emb = embedder.embed(self.dm)
         d = {
             'factor 1': emb[:, 0],
@@ -58,10 +60,10 @@ class Embedding2DScatter(DistanceMatrixPlotter):
         plt.show()
 
 class EmbeddingPairsPlotter(DistanceMatrixPlotter):
-    titlestring = "%s embedding of %s distance matrix Pairs Plot"
+    titlestring = "%s %s Embedding Pairs Plot under %s metric"
 
     def plot(self, embedder):
-        title = self.titlestring % (embedder.embedding_name, self.metric_name)
+        title = self.titlestring % (self.dataset_name, embedder.embedding_name, self.metric_name)
         emb = embedder.embed(self.dm)
         Pdf = pd.DataFrame(emb, columns = ["factor %s"%x for x in range(1, emb.shape[1] + 1)])
         Pdf[self.label_name] = self.label
@@ -69,3 +71,33 @@ class EmbeddingPairsPlotter(DistanceMatrixPlotter):
         plt.subplots_adjust(top=0.9)
         plt.suptitle(title)
         plt.show()
+
+class EmbeddingParallelCoordinatePlotter(DistanceMatrixPlotter):
+    titlestring = "%s %s Embedding Parallel Coordinate Plot under %s metric"
+
+    def plot(self, embedder):
+        title = self.titlestring % (self.dataset_name, embedder.embedding_name, self.metric_name)
+        emb = embedder.embed(self.dm)
+        D = emb.T
+        d, n = D.shape
+        D = D - np.min(D, axis=1).reshape(d, 1)
+        D = D / np.max(D, axis=1).reshape(d, 1)
+        unique_labels = np.unique(self.label)
+        label_to_number = dict(zip(unique_labels, range(1, len(unique_labels) + 1)))
+        dims = [dict(label = "factor %s"%(x + 1),
+                values = D[x, :]) for x in range(embedder.num_components)]
+        line = dict(color = [label_to_number[x] for x in self.label],
+                    cmin = 0,
+                    cmax = len(unique_labels),
+                    colorscale = "Jet",
+                    showscale=True,
+                    colorbar = dict(tickmode = "array",
+                                    ticktext = unique_labels,
+                                    tickvals = [label_to_number[x] for x in unique_labels]))
+        trace = go.Parcoords(line = line, dimensions = list(dims))
+        data = [trace]
+        layout = go.Layout(
+            title=title
+        )
+        fig = dict(data = data, layout = layout)
+        iplot(fig)
