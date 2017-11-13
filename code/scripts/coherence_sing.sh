23cc407453bd38a28b62a97aa65e6aef4dec527e
Nitin Kumar <nkumar14@jhu.edu>
2017-11-13 13:46:19 -0500

    Same as previous commit but with remembering to add the files

---
+++ code/scripts/coherence_sing.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash -e
+
+./memlog.sh > ./mem.txt &
+memkey=$!
+python cpulog.py ./cpu.txt &
+cpukey=$!
+./disklog.sh > ./disk.txt &
+diskkey=$!
+
+python coherence_single.py
+
+kill $memkey $cpukey $diskkey
