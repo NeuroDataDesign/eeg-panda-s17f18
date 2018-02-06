b73541ec9906aa100e80d970956cc1c155698d75
Nitin Kumar <nkumar14@jhu.edu>
2018-02-06 14:57:12 -0500

    publishing updated docs...

--- code/scripts/disklog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-# run this in the background with nohup ./disklog.sh > disk.txt &
-# written by eric bridgeford
-
-while true; do
-    echo "$(du -s $1 | awk '{print $1}')"
-    sleep 30
-done
